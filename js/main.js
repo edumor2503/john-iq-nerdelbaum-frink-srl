87f7768467e893de7062f71c02597f1f2f3c9b47
Eduardo Moreno <167011235+edumor2503@users.noreply.github.com>
2024-06-13 22:55:30 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+	var y = document.getElementById("registrar");
+	var z = document.getElementById("elegir");
+
+	function login(){
+		x.style.left = "10px";
+		y.style.left = "450px";
+		z.style.left = "0px";
+	}
+	
+    function registrar(){
+        x.style.left = "-500px"; 
+        y.style.left = "-10px"; 
+        z.style.left = "120px"; 
+    }	
